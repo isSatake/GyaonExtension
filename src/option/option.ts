e84c85fa9fd7b382e5b00991da14376091023e82
hykwtakumin <hykwtakumin@gmail.com>
2018-06-04 17:22:31 +0900

    * add form and scripts to option page

---
+++ src/option/option.ts
@@ -0,0 +1,10 @@
+window.onload = function() {
+    console.log("this is option page");
+
+    const idForm = document.getElementById("idForm");
+    const idButton = document.getElementById("idButton");
+
+    idButton.addEventListener('click', function (event) {
+        //idFormに入力された文字列をIDとして取得する
+    })
+};
